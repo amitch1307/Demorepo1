d1a94c1f432746e4590337cac1829869dbe61b95
amitch1307 <81789732+amitch1307@users.noreply.github.com>
2021-07-27 11:33:22 +0530

    Update Class1.java

--- src/main/java/BaseClass/Class1.java
+++ src/main/java/BaseClass/Class1.java
@@ -5,6 +5,8 @@ public class Class1 {
 	public static void main(String[] args) {
 		 System.out.println("class 1");
 		 System.out.println("class 3");
+		 System.out.println("class 4");
+		 System.out.println("class 5");
 	}
 
 }
