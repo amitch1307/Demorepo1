620f8e09e156ff0bd248da928b43fb8ce4225061
Prashsti Amit ji <amitsingh.qa@gmail.com>
2021-07-27 09:30:38 +0530

    update code to git

--- src/main/java/BaseClass/Class1.java
+++ src/main/java/BaseClass/Class1.java
@@ -4,7 +4,7 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		 System.out.println("class 1");
-
+		 System.out.println("class 3");
 	}
 
 }
--- src/main/java/BaseClass/Class2.java
+++ src/main/java/BaseClass/Class2.java
@@ -4,6 +4,7 @@ public class Class2 {
 
 	public static void main(String[] args) {
 		 System.out.println("class 2");
+		 System.out.println("class3");
 
 	}
 
